e15aa5a6bb862e93eae7abf27f2c9e7b50422e6b
JayBrothers <152915486+JayBrothers@users.noreply.github.com>
2024-02-28 16:11:09 -0500

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    public Person(String firstname, String lastName){
+        this.firstName = firstname;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;}
+    public String getLastName() {
+        return lastName;
+        }
+    }
+
